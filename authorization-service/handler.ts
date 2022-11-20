457af1a11f3084dfa0f9e10829d5a5713a93effc
Natallia Sasinovich <natallia_sasinovich@epam.com>
2022-11-20 19:47:15 +0100

    init authorization service

---
+++ authorization-service/handler.ts
@@ -0,0 +1,7 @@
+// import * as handlers from '@/functions';
+
+console.log('process.env: ', process.env);
+
+// export const basicAuthorizer = handlers.importProductsFile(productImportService);
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const basicAuthorizer = () => {};
